d8cafc1ed9416beca0a397d16e9f926ed1591ab2
ajungmann <alfonsojungmann@hotmail.com>
2015-09-24 16:21:45 -0300

    Code in R

--- Plot1.R
+++ Plot1.R
@@ -1,12 +1,20 @@
 
-####READ BOTH DATASETS#######
+
+# Load the NEI & SCC data frames.
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
-###SUMMARIZE#################
-PM25_Year <- tapply(NEI$Emissions, NEI$year, sum)
-###PLOT######################
-png("plot1.png")
-plot(names(PM25_Year), PM25_Year, type="l",
-     xlab="Year", ylab=expression("Total" ~ PM[2.5] ~ "Emissions (tons)"),
-     main=expression("Total US" ~ PM[2.5] ~ "Emissions by Year"))
+
+# Aggregate by sum the total emissions by year
+aggTotals <- aggregate(Emissions ~ year,NEI, sum)
+
+png("plot1.png",width=480,height=480,units="px",bg="transparent")
+
+barplot(
+    (aggTotals$Emissions)/10^6,
+    names.arg=aggTotals$year,
+    xlab="Year",
+    ylab="PM2.5 Emissions (10^6 Tons)",
+    main="Total PM2.5 Emissions From All US Sources"
+)
+
 dev.off()
